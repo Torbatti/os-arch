6e84fb444259b5c650214f3534676559ea0d0bcd
torbatti <torbattii@gmail.com>
2023-02-12 11:12:21 +0330

    update check_list function removed useless commands

--- scripts/arch.sh
+++ scripts/arch.sh
@@ -16,25 +16,48 @@ declare -a color=(
   [2]="\e[31m"
   [3]="\e[32m"
   [4]="\e[34m"
+  [5]="\e[37m"
+  [6]="\e[90m"
   [100]="\e[0m"
 )
 
+function check_list() {
+  name=$1[@]
+  a=("${!name}")
+  echo -e -n " ${color[3]}$1:"
+  echo -n " "
+  for i in "${a[@]}";
+    do
+      # Check If The Program is installed
+      echo -e -n ""
+      if ! command -v $i &> /dev/null
+        then
+          echo -e -n " ${color[6]}$i"
+        else 
+          echo  -e -n " ${color[5]}$i"
+      fi
+    done
+  echo ""
+}
 
-# Browsers
+
+# echo -e "${color[2]}Checking Dev Tools ${color[100]}"
+# # read -p 'Enter Number 1: ' number1 
+# # read -p 'Enter Number 2: ' number2
+
 declare -a browser=(
   [0]=firefox
   [1]=chromium
   [2]=brave
   [3]=librewolf
 )
+check_list browser
 
-
-# Terminal Emulators
-declare -a term_emulat=(
+declare -a term=(
   [0]=kitty
-  [1]=kitty
-  [2]=alacrity
+  [1]=alacritty
 )
+check_list term
 
 # Codecs
   #sudo pacman -S a52dec faac faad2 flac jasper lame libdca libdv libmad libmpeg2 libtheora libvorbis libxv wavpack x264 xvidcore gstreamer0.10-plugins
@@ -61,6 +84,10 @@ declare -a term_emulat=(
   #Chaotic-AUR
     #link: https://aur.chaotic.cx/
 #
+declare -a aur_helpers=(
+  [0]=yay
+)
+check_list aur_helpers
 
 # App Bundlers
   #snap
@@ -74,68 +101,62 @@ declare -a term_emulat=(
   #protonup
 #
 
+# IDE
+declare -a ide=(
+  [0]=nano
+  [1]=vim
+  [2]=neovim
+  [3]=emacs
+  [4]=code
+  [5]=atom
+)
+check_list ide
 
-# IDES
-  #vscode
-  #atom
-  #vim
-  #emacs
-#
+declare -a note=(
+  [0]=obsidian
+)
+check_list note
 
-# Note
-  #obsidian
-#
+
 
 # Internet Download Managers
   #Motrix
     #motrix --no-sandbox
-#
+declare -a idm=(
+  [0]=motrix
+)
+check_list idm
+
 
 # Devel
-  #npm
-  #rust
-  #nim
-  #python3
-  #php
-  #mysql
-  #sqlite
-  #apachi
-  #nginx
-  #docker
   #podman
   #buildah
 #
+
+declare -a data_bases=(
+  [0]=mysql
+  [1]=sqlite3
+)
+check_list data_bases
+
+
 declare -a dev_tools=(
   [0]=gcc
-  [1]=rustup
+  [1]=npm
+  [2]=apache
+  [3]=nginx
+  [4]=docker
 )
-
-function check_list() {
-  name=$1[@]
-  a=("${!name}")
-  # a=$1[@]
-  echo -e "${color[4]}$1:"
-  for i in "${a[@]}";
-    do
-      # echo -e "$i \n"
-      # Chech If The Program is installed
-      echo -e -n " "
-      if ! command -v $i &> /dev/null
-        then
-          echo -e -n "${color[2]}$i "
-        else 
-          echo  -e -n "${color[3]}$i "
-      fi
-    done
-  echo ""
-}
 check_list dev_tools
 
+declare -a progrraming_language=(
+  [0]=rustup
+  [1]=nim
+  [2]=python3
+  [3]=php
+  [4]=zig
+  [5]=go
+)
+check_list progrraming_language
 
-
-# echo -e "${color[2]}Browsers:${color[100]}"
-# echo " ${browser[@]}"
-
-# echo -e "${color[2]}Checking Dev Tools ${color[100]}"
-# # read -p 'Enter Number 1: ' number1 
-# # read -p 'Enter Number 2: ' number2
+echo ""
